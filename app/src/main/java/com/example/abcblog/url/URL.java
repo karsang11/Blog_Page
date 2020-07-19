1b534127f632545ba5bff39069093f6b36d7b3ed
Jimmy483 <49531279+Jimmy483@users.noreply.github.com>
2020-07-19 17:17:27 +0545

    Add files via upload

---
+++ app/src/main/java/com/example/abcblog/url/URL.java
@@ -0,0 +1,22 @@
+package com.example.abcblog.url;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class URL {
+    public static final String base_url = "http://10.0.2.2:3000/";
+    //try for phone
+    // public static final String base_url = "http://192.168.1.90:3000/";
+    //public static final String base_url = "http://172.100.100.5:3000/";
+    //public static final String base_url = "http://192.168.1.11:3000/";
+    public static String token = "Bearer ";
+    public static String imagePath = base_url + "/uploads/";
+
+    public static Retrofit getRetrofit() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+}
